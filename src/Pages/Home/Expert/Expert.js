d369fa06ca6282c59ad51aff8e96f935649c32dc
nurulamin092 <mnurulamin092@gmial.com>
2022-04-14 10:38:25 +0600

    update navbar  responsive

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -5,11 +5,11 @@ const Expert = ({ expert }) => {
     return (
         <div className='gx-5 gy-5 col-sm-12 col-md-4 col-ls-4'>
             <div className="card " style={{ width: '18rem' }}>
-                <img src={img} class="card-img-top" alt="..." />
-                <div class="card-body">
+                <img src={img} className="card-img-top" alt="..." />
+                <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
         </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../../../images/logo.png';
 const Header = () => {
     return (
         <header>
-
-            <Link to='/home'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Navbar bg="primary" variant="dark">
+            <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
                 <Container>
-                    <Navbar.Brand href='#home'>
-                        <img src={logo} height='30px' alt="" />
-                    </Navbar.Brand>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
+                    <Navbar.Brand href="#home">Car Services</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
 
