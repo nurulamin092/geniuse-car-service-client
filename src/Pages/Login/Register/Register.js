016fc5876f1ed452e2f0fb66729b86fe04fd773b
nurulamin092 <mnurulamin092@gmial.com>
2022-04-14 14:55:31 +0600

    update register form

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,21 +3,24 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
 const Register = () => {
     const navigate = useNavigate();
-    const navigateLogin = (e) => {
+    const navigateLogin = () => {
         navigate('/login')
+    }
+    const handleRegister = (e) => {
+        e.preventDefault();
     }
     return (
         <div className='register-form'>
-            <h3>Please Register</h3>
-            <form>
+            <h3 style={{ textAlign: 'center' }}>Please Register</h3>
+            <form onSubmit={handleRegister}>
                 <input type='text' name='name' id='' placeholder='Your Name'></input>
                 <input type="email" name="email" id="" placeholder='Your Email' required />
                 <input type="password" name="password" id="" placeholder='Your Password' required />
                 <input type="submit" value="Register" />
             </form>
-            <p>Already Register? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+            <p>Already have a account? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
 
-        </div>
+        </div >
     );
 };
 
