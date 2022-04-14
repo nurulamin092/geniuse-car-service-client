62e35e1f1d6d74a1ec64897eb45b71ce9449ee56
nurulamin092 <mnurulamin092@gmial.com>
2022-04-14 14:40:44 +0600

    create register form

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 const Register = () => {
+    const navigate = useNavigate();
+    const navigateLogin = (e) => {
+        navigate('/login')
+    }
     return (
-        <div>
+        <div className='register-form'>
             <h3>Please Register</h3>
+            <form>
+                <input type='text' name='name' id='' placeholder='Your Name'></input>
+                <input type="email" name="email" id="" placeholder='Your Email' required />
+                <input type="password" name="password" id="" placeholder='Your Password' required />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already Register? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+
         </div>
     );
 };
