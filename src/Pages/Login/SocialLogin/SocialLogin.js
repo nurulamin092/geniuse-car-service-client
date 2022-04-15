fad6c58ac0560b6b247479b9da9449d5ae63df1c
nurulamin092 <mnurulamin092@gmial.com>
2022-04-15 06:02:22 +0600

    update auth in google and facebook and github account

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,7 +2,16 @@ import React from 'react';
 import google from '../../../images/social/google.png'
 import facebook from '../../../images/social/facebook.png'
 import github from '../../../images/social/github.png';
+import { useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 const SocialLogin = () => {
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const [signInWithFacebook] = useSignInWithFacebook(auth);
+    const [signInWithGithub] = useSignInWithGithub(auth);
+
+    /*  const handleGoogleSignIn = () => {
+ 
+     } */
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -11,15 +20,15 @@ const SocialLogin = () => {
                 <div style={{ height: '1px' }} className='bg-primary w-50'></div>
             </div>
             <div>
-                <button className='btn btn-light w-50 my-2 d-block mx-auto'>
+                <button onClick={() => signInWithGoogle()} className='btn btn-light w-50 my-2 d-block mx-auto'>
                     <img style={{ width: '30px' }} src={google} alt="" />
                     <span className='px-2'>  Google Sign In</span>
                 </button>
-                <button className='btn btn-primary w-50 my-2 d-block mx-auto'>
+                <button onClick={() => signInWithFacebook()} className='btn btn-primary w-50 my-2 d-block mx-auto'>
                     <img style={{ width: '30px' }} src={facebook} alt="" />
                     <span className='px-2'>  Facebook Sign In</span>
                 </button>
-                <button className='btn btn-info w-50 my-2 d-block mx-auto'>
+                <button onClick={() => signInWithGithub()} className='btn btn-info w-50 my-2 d-block mx-auto'>
                     <img style={{ width: '30px' }} src={github} alt="" />
                     <span className='px-2'>  Github Sign In</span>
                 </button>
